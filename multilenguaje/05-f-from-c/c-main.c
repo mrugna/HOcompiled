0275627a33ab64280c6aa0b56ad757fb826c18cc
Martin <martinrugna@gmail.com>
2015-11-05 15:46:41 -0300

    Terminado ejercicio 5

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,20 @@
 
 #include <stdio.h>
-#include "c-sum.h"
+/* #include "c-sum.h" */
 
 #define NUM 200
 
+extern void sum_abs_(int *data, int *num, int *out);
+
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
+    int data[NUM], num, i, out;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
-
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &out);
+    printf("sum=%d\n", out);
     return 0;
 }
